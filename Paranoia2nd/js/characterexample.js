c5d10df06fac5d0d07216cd77d84b381f81c164f
hevesg <hevesg@users.noreply.github.com>
2014-08-28 18:13:21 +0100

    Create characterexample.js

---
+++ Paranoia2nd/js/characterexample.js
@@ -0,0 +1,24 @@
+{
+  "character": {
+    "metadata": {
+      "created": "2014-08-28 18:06",
+      "modified": "2014-08-28 18:06"
+    },
+    "general": {
+      "name": {
+        "base": "Gabor",
+        "area": "XOR",
+        "clone": "1"
+      }
+    },
+    "attributes": {
+      "strength": "6",
+      "endurance": "15",
+      "agility": "8",
+      "moxie": "6",
+      "chutzpah": "15",
+      "mech": "8",
+      "mutant_power": "8"
+    }
+  }
+}
