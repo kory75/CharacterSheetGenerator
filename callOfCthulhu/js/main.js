7ff1652b6a3f8c73282ff8a14fa66046241bf0c7
Laszlo Heves <laszlo.heves@gawker.com>
2014-09-01 00:35:31 +0100

    Add `statistics` to charsheet

--- callOfCthulhu/js/main.js
+++ callOfCthulhu/js/main.js
@@ -1,12 +1,14 @@
 angular.module('cthulhuApp', [])
-	.controller('GenericController', ['$scope', function($scope) {
+	.controller('ChatSheetController', ['$scope', function($scope) {
 // TODO: should be a more delicate way tho...
 	$scope.generic = {};
+	$scope.statistic = {};
 
 // console.log actual state of the `generic` model
 // only here for demonstrational purposes
 	$scope.onDebugger = function () {
 		console.log(this.generic);
+		console.log(this.statistic);
 		//debugger;
 	};
 }]);
