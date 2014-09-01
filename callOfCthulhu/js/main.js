fe0e07ad5efabda2cfed7c45ae0d3518787e33e9
Laszlo Heves <laszlo.heves@gawker.com>
2014-09-01 23:13:37 +0100

    Add temporary random values

--- callOfCthulhu/js/main.js
+++ callOfCthulhu/js/main.js
@@ -1,4 +1,16 @@
+// global variable for random values
+// TODO: should be a more delicate way tho...
+randomValues = {
+	name: ['Brogam Moore', 'Arthur R. Sulivan', 'Kyle Lukowsky', 'Benjamin T. Thomson'],
+	age: Math.floor(Math.random()*100),
+	occupation: ['advocat', 'archeologist', 'occultist', 'highschool teacher'],
+	sex: ['male', 'female'],
+	nationality: ['english', 'american', 'german'],
+	redisence: ['Boston, MA', 'Arkham, MA', 'Quochag, MA']
+};
+
 angular.module('cthulhuApp', [])
+
 	.controller('CharSheetController', ['$scope', function($scope) {
 // TODO: should be a more delicate way tho...
 	$scope.generic = {};
@@ -9,6 +21,10 @@ angular.module('cthulhuApp', [])
 	$scope.onDebugger = function () {
 		console.log(this.generic);
 		console.log(this.statistic);
-		//debugger;
+		// debugger;
+	};
+
+	$scope.onRandom = function () {
+		debugger;
 	};
 }]);
