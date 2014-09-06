ab4da30c3769b74ac1c7e3c362fcba7adf74bcbe
Laszlo Heves <laszlo.heves@gawker.com>
2014-09-07 01:19:00 +0200

    Add form as JSONish object

--- callOfCthulhu/js/main.js
+++ callOfCthulhu/js/main.js
@@ -4,6 +4,46 @@ angular.module('cthulhuApp', [])
 // TODO: should be a more delicate way tho...
 	$scope.generic = {};
 	$scope.statistic = {};
+	var form = [
+		{
+			fieldsetName: 'General Information',
+			fields: [
+				{
+					name: 'name',
+					type: 'text',
+					label: 'Name',
+					description: '',
+					size: 'medium' //indicates the size of the field
+				},
+				{
+					name: 'occupation',
+					type: 'text',
+					label: 'Occupation',
+					description: '',
+					size: 'medium'
+				},
+			]
+		},
+		{
+			fieldsetName: 'Statistics',
+			fields: [
+				{
+					name: 'str',
+					type: 'number',
+					label: 'STR',
+					description: 'Strength',
+					dice: '3d6'
+				},
+				{
+					name: 'dex',
+					type: 'number',
+					label: 'DEX',
+					description: 'Dexterity',
+					dice: '3d6'
+				},
+			]
+		}
+	];
 
 // console.log actual state of the `generic` model
 // only here for demonstrational purposes
